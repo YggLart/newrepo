a29862683f2d531e935960005416f3d546d18f0b
Ygor Goulart <ygorgm95@gmail.com>
2022-05-14 07:56:34 -0300

    Early responsiveness

---
+++ main.js
@@ -0,0 +1,7 @@
+var burgerMenu = document.querySelector(".fa-bars");
+var sideMenu = document.querySelector(".nav-links");
+
+function responsiveMenu() {
+    burgerMenu.classList.add(".fa-plus");
+    burgerMenu.classList.remove(".fa-bars");
+}
