eb7bc0397bc269633848fc5e37d553d6cf988aef
Ygor Goulart <ygorgm95@gmail.com>
2022-05-15 15:57:04 -0300

    small changes

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ var burgerMenu = document.querySelector(".fa-bars");
 var sideMenu = document.querySelector(".nav-links");
 
 function responsiveMenu() {
-    burgerMenu.classList.add(".fa-plus");
-    burgerMenu.classList.remove(".fa-bars");
+    sideMenu.classList.toggle("nav-active");
 }
