a1746f7b20653301cace71d15c6b04fc18084d89
Ygor Goulart <101728385+YggLart@users.noreply.github.com>
2022-05-16 10:22:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
-var burgerMenu = document.querySelector(".fa-bars");
-var sideMenu = document.querySelector(".nav-links");
+var links = document.getElementById("nav-links");
+var burgerButton = document.querySelector(".fa");
 
-function responsiveMenu() {
-    burgerMenu.classList.add(".fa-plus");
-    burgerMenu.classList.remove(".fa-bars");
+function burgerMenu() {
+    if (links.style.transform === "translateX(25rem)") {
+        links.style.transform = "translateX(0)";
+        burgerButton.classList.replace("fa-bars", "fa-plus");
+        burgerButton.style.transform = "rotate(45deg)";
+
+    } else {
+        links.style.transform = "translateX(25rem)";
+        burgerButton.classList.replace("fa-plus", "fa-bars");
+        burgerButton.style.transform = "rotate(0deg)";
+    }
+}
+
+navbarAdjust();
+window.onresize = navbarAdjust;
+
+function navbarAdjust() {
+    if (window.innerWidth >= 800) {
+        links.style.transform = "translateX(0)";
+    }
+
+    if (window.innerWidth <= 800) {
+        links.style.transform = "translateX(25rem)";
+
+        if (links.style.transform === "translateX(25rem)") {
+            burgerButton.classList.replace("fa-plus", "fa-bars");
+            burgerButton.style.transform = "rotate(0deg)";
+
+        } else {
+            burgerButton.classList.replace("fa-bars", "fa-plus");
+            burgerButton.style.transform = "rotate(45deg)";
+        }
+    }
 }
